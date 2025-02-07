78fa2a9ab3793dc5fbea441a14abb50e7e30f86f
Daniel Israel <danny.israel@gmail.com>
2025-02-06 22:00:04 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                lato: ['Lato', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}; 
